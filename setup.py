21b49ca18a51ff5df87d1d9b482c7dfc1445af2e
Andrew Taeoalii <andrewtaeoalii@Andrews-Air.hawaii.rr.com>
2018-06-28 06:29:56 -1000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='uluplot',
+      version='0.1.0',
+      description='Flexible map plot utility based on gmplot',
+      url='http://github.com/ulu5/uluplot',
+      author='ulu5',
+      author_email='ulu_5@yahoo.com',
+      license='MIT',
+      packages=['uluplot'],
+      install_requires=[
+          'requests',
+      ])
